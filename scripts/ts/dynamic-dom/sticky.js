136d9f3a3386d68506d19b2723293ba940d56205
oliviaschotz <oschotz@wustl.edu>
2021-02-07 14:13:32 -0600

    sticky done

--- scripts/ts/dynamic-dom/sticky.js
+++ scripts/ts/dynamic-dom/sticky.js
@@ -1,15 +1,15 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
-var header = document.getElementById("myHeader");
-
-//Get the offset position of the navbar
-var sticky = header.offsetTop;
-
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 export function setupSticky() {
-    console.log("sticky!")
+
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {setupSticky()};
+
+  // Get the header
+  var header = document.getElementById("main-menu-container");
+
+  //Get the offset position of the navbar
+  var sticky = header.offsetTop;
+  console.log("sticky!")
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
